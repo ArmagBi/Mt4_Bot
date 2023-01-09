7c2a1d00f1252931c335aa323962c3e2a6561d2a
Siyavash Momeni <74583375+ArmagBi@users.noreply.github.com>
2023-01-09 11:17:26 +0330

    Add files via upload

---
+++ client.py
@@ -0,0 +1,31 @@
+from threading import Thread
+from time import sleep
+from datetime import datetime
+
+from mt4 import Trader
+
+now = datetime.now
+
+BUY = 0
+SELL = 1
+
+
+class Client(Thread):
+    """ZMQ client thread connecting to MQL server"""
+    def __init__(self, push, pull):
+        self.manager = Trader(push, pull)
+        super(Client, self).__init__()
+
+    def run(self):
+        Thread(target=self.manager.push_listener).start()
+        while True:
+            try:
+                self.manager.data("EURUSD", "H1")
+                print ('{}:{}:{} - Data request sent'.format(now().hour, now().minute, now().second))
+
+                sleep(10)
+            except:
+                continue
+        print ('Disconnecting sockets..')
+        self.con.disconnect()
+        print ('Done')
